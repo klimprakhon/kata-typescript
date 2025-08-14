720ff4d768161ee345155fd1437453a20343a1d9
klimprakhon <klimprakhon@gmail.com>
2025-08-14 09:49:26 +0700

    refactor(roman-numerals): loop over "I" for 1-3 cases

--- src/roman-numerals.ts
+++ src/roman-numerals.ts
@@ -1,9 +1,8 @@
 function convertToRomanNumerals(arabicNumber: number) {
   if (arabicNumber === 4) return "IV";
-  if (arabicNumber === 1) return "I";
-  if (arabicNumber === 3) return "III";
 
-  return "Invalid Number";
+  const romanNumberOne = "I";
+  return romanNumberOne.repeat(arabicNumber);
 }
 
 export default convertToRomanNumerals;
