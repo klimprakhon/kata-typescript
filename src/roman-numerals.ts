b07ed98057ff316576fa7f3a1d164195d01dac64
klimprakhon <klimprakhon@gmail.com>
2025-08-14 10:03:17 +0700

    refactor(roman-numerals): roman number for 5 and 8 cases

--- src/roman-numerals.ts
+++ src/roman-numerals.ts
@@ -1,9 +1,15 @@
 function convertToRomanNumerals(arabicNumber: number) {
-  if (arabicNumber === 8) return "VIII";
-  if (arabicNumber === 5) return "V";
+  const romanNumberOne: string = "I";
+  const romanNumberFive: string = "V";
+
   if (arabicNumber === 4) return "IV";
 
-  const romanNumberOne = "I";
+  if (arabicNumber >= 5) {
+    const roundToRepeat = arabicNumber - 5;
+    const repeatedOneRomanNumbers = romanNumberOne.repeat(roundToRepeat);
+    return romanNumberFive + repeatedOneRomanNumbers;
+  }
+
   return romanNumberOne.repeat(arabicNumber);
 }
 
