1507828991b0c01fc769c771ac9d13b461d3317c
klimprakhon <klimprakhon@gmail.com>
2025-08-20 09:11:22 +0700

    refactor(roman-numerals): using only one while loop then cascade the conditions

--- src/roman-numerals.ts
+++ src/roman-numerals.ts
@@ -6,27 +6,31 @@ function convertToRomanNumerals(arabicNumber: number) {
   const romanNumberFive: string = "V";
   const romanNumberTen: string = "X";
 
-  while (remainValue >= 10) {
-    resultRomanNumber += romanNumberTen;
-    remainValue -= 10;
-  }
-
-  if (remainValue === 9) {
-    resultRomanNumber += "IX";
-    remainValue -= 9;
-  }
-
-  if (remainValue >= 5) {
-    resultRomanNumber += romanNumberFive;
-    remainValue -= 5;
-  }
-
-  if (remainValue === 4) {
-    resultRomanNumber += "IV";
-    remainValue -= 4;
-  }
-
   while (remainValue > 0) {
+    if (remainValue >= 10) {
+      resultRomanNumber += romanNumberTen;
+      remainValue -= 10;
+      continue;
+    }
+
+    if (remainValue === 9) {
+      resultRomanNumber += "IX";
+      remainValue -= 9;
+      continue;
+    }
+
+    if (remainValue >= 5) {
+      resultRomanNumber += romanNumberFive;
+      remainValue -= 5;
+      continue;
+    }
+
+    if (remainValue === 4) {
+      resultRomanNumber += "IV";
+      remainValue -= 4;
+      continue;
+    }
+
     const repeatedOneRomanNumbers = romanNumberOne.repeat(remainValue);
     resultRomanNumber += repeatedOneRomanNumbers;
     remainValue -= remainValue;
