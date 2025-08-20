1aa1fd931e59cc2b1387014317a08f95d971c166
klimprakhon <klimprakhon@gmail.com>
2025-08-20 09:32:28 +0700

    refactor(roman-numerals): using romanNumerals to store all roman numerals instead of separating variables

--- src/roman-numerals.ts
+++ src/roman-numerals.ts
@@ -2,52 +2,54 @@ function convertToRomanNumerals(arabicNumber: number) {
   let remainValue = arabicNumber;
   let resultRomanNumber = "";
 
-  const romanNumberOne: string = "I";
-  const romanNumberFour: string = "IV";
-  const romanNumberFive: string = "V";
-  const romanNumberNine: string = "IX";
-  const romanNumberTen: string = "X";
-  const romanNumberForty: string = "XL";
-  const romanNumberFifty: string = "L";
+  const romanNumerals = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+  };
 
   while (remainValue > 0) {
     if (remainValue >= 50) {
-      resultRomanNumber += romanNumberFifty;
+      resultRomanNumber += romanNumerals[50];
       remainValue -= 50;
       continue;
     }
 
     if (remainValue === 40) {
-      resultRomanNumber += romanNumberForty;
+      resultRomanNumber += romanNumerals[40];
       remainValue -= 40;
       continue;
     }
 
     if (remainValue >= 10) {
-      resultRomanNumber += romanNumberTen;
+      resultRomanNumber += romanNumerals[10];
       remainValue -= 10;
       continue;
     }
 
     if (remainValue === 9) {
-      resultRomanNumber += romanNumberNine;
+      resultRomanNumber += romanNumerals[9];
       remainValue -= 9;
       continue;
     }
 
     if (remainValue >= 5) {
-      resultRomanNumber += romanNumberFive;
+      resultRomanNumber += romanNumerals[5];
       remainValue -= 5;
       continue;
     }
 
     if (remainValue === 4) {
-      resultRomanNumber += romanNumberFour;
+      resultRomanNumber += romanNumerals[4];
       remainValue -= 4;
       continue;
     }
 
-    const repeatedOneRomanNumbers = romanNumberOne.repeat(remainValue);
+    const repeatedOneRomanNumbers = romanNumerals[1].repeat(remainValue);
     resultRomanNumber += repeatedOneRomanNumbers;
     remainValue -= remainValue;
   }
